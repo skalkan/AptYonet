e7ffac9ff9d7f3692235c34081f17c6e0a0c2a04
serdar <skalkan@gmail.com>
2018-06-10 01:31:34 +0300

    Copied from Dan Wahlin's seed project

---
+++ Client/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { DataService } from '../core/services/data.service';
+
+@Component({
+    selector: 'home',
+    templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+
+    message: string;
+
+    constructor(private dataService: DataService) { }
+
+    ngOnInit() {
+        this.dataService.getMessage().subscribe((message: string) => {
+            this.message = message;
+        })
+    }
+}
